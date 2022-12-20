aa3eb0c362b91f901bbbd8047e88bf7368103654
Dankelberg <josedaniel1996nb@gmail.com>
2022-12-20 22:52:25 +0000

    HistoryApp - first commit

---
+++ src/main/java/com/dankelberg/historyApp/HistoryAppApplication.java
@@ -0,0 +1,13 @@
+package com.dankelberg.historyApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HistoryAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HistoryAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dankelberg/historyApp/HistoryAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dankelberg.historyApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HistoryAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
